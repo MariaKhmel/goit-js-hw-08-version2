52d4f39ca6aa88836aec78fb3178092bfa2ecdcf
MariaKhmel <mariakhmel8@gmail.com>
2023-10-08 00:26:13 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
-console.log(galleryItems);
+
+const refs = {
+    gallery: document.querySelector('.gallery'),
+
+}
+
+refs.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems))
+function createGalleryMarkup(items) {
+    return items.map(({ original, preview, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a');
+
+// refs.gallery.addEventListener('click', pictureClick);
+
+// function pictureClick(e) {
+//     e.preventDefault();
+//     const pictureUrl = e.target.dataset.source;
+//     // if (e.target.classList.contains('gallery__image')) {
+//     //     console.log('click'); 
+//     // }
+//     let gallery = new SimpleLightbox('.gallery a');
+// }
+
+
+
